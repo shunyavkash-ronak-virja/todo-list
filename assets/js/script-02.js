3db7618f8fdd4978f88706c5331f846fc82adb3d
Ronak Virja <ronak.virja@shunyavkash.com>
2026-06-12 11:56:50 +0530

    update: remove li structure from js and create in html

--- assets/js/script-02.js
+++ assets/js/script-02.js
@@ -2,6 +2,7 @@ const todoInput = document.querySelector(".todo-input");
 const addBtn = document.querySelector(".todo-add-btn");
 const todoList = document.querySelector(".todo-list");
 const clearAllBtn = document.querySelector(".clear-all-btn");
+const todoTemplate = document.querySelector("#todo-template");
 
 // Load todos when page loads
 document.addEventListener("DOMContentLoaded", loadTodos);
@@ -28,27 +29,9 @@ todoInput.addEventListener("keydown", (event) => {
 
 // Function to create todo HTML
 function addTodoToDOM(taskText) {
-  const newTodoHTML = `
-        <li>
-            <span class="todo-list-task">${taskText}</span>
-            <div class="todo-item-btn-wrapper">
-                <button class="todo-item-btn todo-edit-btn flex-1" type="button" onclick="editTodo(this)">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="todo-item-btn-icon lucide lucide-pencil-icon lucide-pencil">
-                    <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
-                    <path d="m15 5 4 4" />
-                    </svg>
-                </button>
-                <button class="todo-item-btn todo-delete-btn flex-1" type="button" onclick="deleteTodo(this)">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="todo-item-btn-icon lucide lucide-trash-icon lucide-trash">
-                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
-                    <path d="M3 6h18" />
-                    <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-                    </svg>
-                </button>
-            </div>
-        </li>
-  `;
-  todoList.insertAdjacentHTML("beforeend", newTodoHTML);
+  const todoClone = todoTemplate.content.cloneNode(true);
+  todoClone.querySelector(".todo-list-task").textContent = taskText;
+  todoList.appendChild(todoClone);
 }
 
 // Save todo to localStorage
